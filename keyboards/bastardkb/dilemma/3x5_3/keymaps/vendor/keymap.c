aaf59623630a36247ca33e148ca56b10e1d7bdd9
Ivan <58172188+id-b3@users.noreply.github.com>
2025-06-03 16:12:00 +0100

    fix

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/vendor/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,          KC_SCLN, KC_Y,    KC_U,    KC_L,    KC_J,
     HM_A,    HM_R,    HM_S,    HM_T,    KC_D,          HM_O,    HM_I,    HM_E,    HM_N,    KC_H,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_K, 
-    (_NAVFUNC, KC_ENT), LT(_SYMNUM, KC_SPC), KC_ESC,   KC_BSPC, LT(_NAVFUNC, KC_TAB), KC_MUTE
+    LT(_NAVFUNC, KC_ENT), LT(_SYMNUM, KC_SPC), KC_ESC,   KC_BSPC, LT(_NAVFUNC, KC_TAB), KC_MUTE
   ),
 
   [_SYMNUM] = LAYOUT_split_3x5_3(
